b61b00d1d37bbf4b46160754d33724b637e4ef6c
KingSpike <87199787+KingSpike@users.noreply.github.com>
2021-09-21 08:52:14 -0400

    Added directories

--- src/main/java/delLater.java
+++ src/main/java/delLater.java
@@ -1,2 +1,16 @@
-public class delLater {
+import java.net.URLConnection;
+import java.util.*;
+
+public class delLater() {
+    public String  website = "https://www.mediawiki.org/w/api.php?";
+    public String getRequest = "api.php?action=query&prop=";
+
+    public String getRequest2 = "revisions&titles=API|Main%20Page&rvprop=timestamp|user|comment&rvslots=main&formatversion=2";
+
+    Scanner input = new Scanner(System.in);
+
+    public String finalFormRequest = getRequest + input.toString() + getRequest;
+
+
+
 }
--- src/test/java/delLaterTest.java
+++ src/test/java/delLaterTest.java
@@ -1,2 +1,12 @@
+
+import org.junit.jupiter.api.Test;
+
 public class delLaterTest {
+    @Test
+    public boolean recentChangesCollectionTest(String recentChng){
+        if (){
+
+        }
+        return True;
+    }
 }
