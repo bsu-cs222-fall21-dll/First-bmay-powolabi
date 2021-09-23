c1cf9bb5140ae70e377f5cdab4f65cac31cc875e
KingSpike <87199787+KingSpike@users.noreply.github.com>
2021-09-23 15:51:46 -0400

    Added directories

--- src/main/java/wikipediaCollect.java
+++ src/main/java/wikipediaCollect.java
@@ -1,11 +1,12 @@
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.*;
-import JsonPath;
+import java.util.Scanner;
+import static com.jayway.jsonpath.JsonPath;
 
 public class wikipediaCollect {
     //public String charset = "UTF-8";
+    public String wikiGetRequest = "api.php?action=query&prop=revisions&titles=API:Geosearch&rvlimit=30&rvslots=main&rvprop=timestamp|user|comment&rvdir=newer&rvstart=2021-09-23T00:00:00Z";
     public String url = "https://www.mediawiki.org/w/api.php?";
     public String getRequest = "action=query&prop=";
     public String getRequest2 = "revisions&titles=API|Main%20Page&rvprop=timestamp|user|comment&rvslots=main&formatversion=2";
