a0ad1574e6d2d2dbad499b0a1476af339e534501
KingSpike <87199787+KingSpike@users.noreply.github.com>
2021-10-05 15:25:23 -0400

    Added directories

--- src/main/java/bsu/edu/cs222/main.java
+++ src/main/java/bsu/edu/cs222/main.java
@@ -3,12 +3,12 @@ package bsu.edu.cs222;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.Scanner;
-
 import com.jayway.jsonpath.JsonPath;
+
 public class main {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.println("Which article would you like to see the last editors of?");
+        System.out.println("Which article would you like to see the last editors of? : ");
         String input = reader.nextLine();
 
         if (input.isEmpty()) {
