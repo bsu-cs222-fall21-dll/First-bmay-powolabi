444b574b28d06ac86f45b6a4e84e9bc2389e99bf
KingSpike <87199787+KingSpike@users.noreply.github.com>
2021-10-05 15:02:58 -0400

    Added directories

--- src/main/java/bsu/edu/cs222/main.java
+++ src/main/java/bsu/edu/cs222/main.java
@@ -2,11 +2,19 @@ package bsu.edu.cs222;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.Scanner;
+
 import com.jayway.jsonpath.JsonPath;
 public class main {
     public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Which article would you like to see the las editors of?");
+        String input = reader.next();
 
-        String input = "rabbit";
+        if (input.isEmpty()) {
+            System.err.println("error code 1");
+            System.exit(0);
+        }
 
         try {
             InputStream jsonData = wikipediaCollect.buildConnection(input);
--- src/main/java/bsu/edu/cs222/wikipediaParser.java
+++ src/main/java/bsu/edu/cs222/wikipediaParser.java
@@ -21,10 +21,16 @@ public class wikipediaParser {
             System.out.println(time.toString());
             ArrayList<String> userJpath = JsonPath.read(user, "$..user");
             ArrayList<String> timeJpath = JsonPath.read(time, "$..timestamp");
+            ArrayList<String> redirect = JsonPath.read(time, "$..timestamp");
             System.out.println(timeJpath);
-            for(int i = 0; i < timeJpath.size(); i++){
-                System.out.println("Time stamp: " + timeJpath.get(i));
-                System.out.println("User Name: " + userJpath.get(i));
+            if(!timeJpath.isEmpty()) {
+                for (int i = 0; i < timeJpath.size(); i++) {
+                    System.out.println("Time stamp: " + timeJpath.get(i));
+                    System.out.println("User Name: " + userJpath.get(i));
+                }
+            } else {
+                System.err.println("There is no data given for the provided search term: err code 2");
+                System.exit(0);
             }
 
         } catch (IOException e) {
